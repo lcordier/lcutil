047effe6fddf2eafc1ef6f4856f2ca76c15f6d03
Louis Cordier <lcordier@gmail.com>
2023-10-22 12:26:27 +0200

    Cosmetic changes, documentation.

--- lcutil/util_password.py
+++ lcutil/util_password.py
@@ -1,6 +1,51 @@
 #!/usr/bin/env python
 
 """ Common password/passphrase functions.
+
+    https://en.wikipedia.org/wiki/Password_strength
+    https://explainxkcd.com/wiki/index.php/936:_Password_Strength
+
+    https://blog.jgc.org/2010/12/write-your-passwords-down.html
+    https://en.wikipedia.org/wiki/Tabula_recta
+    http://yannesposito.com/Scratch/en/blog/Password-Management/
+    https://www.borderwallets.com/docs/the-solution
+
+    https://www.netmux.com/blog/one-time-grid
+
+    https://en.wikipedia.org/wiki/Diceware
+    https://theworld.com/~reinhold/diceware.html
+    https://arstechnica.com/information-technology/2014/03/diceware-passwords-now-need-six-random-words-to-thwart-hackers/
+    https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases
+    https://diceware.dmuth.org/
+    https://theworld.com/%7Ereinhold/dicewarekit.html
+    https://github.com/agreinhold/Diceware-word-lists
+
+Why diceware?
+
+Which is more memorable? Da6ee^ch.aij or mock omen laugh weary dy glen?
+
+The entropy offered by Diceware is 12.9 bits per word - log2(7776) - so the six-word passphrase above has an entropy of 77.4 bits.
+
+Compare this to choosing 10 letters at random: the entropy of that is 10 * log2(26) = 47.0 bits.
+
+If you were to use ASCII printable characters, you'd need at least 12 characters to surpass a six word memorable diceware password.
+
+Diceware is a method for creating passphrases, passwords, and other cryptographic variables using an ordinary die from a pair of dice as a hardware random number generator. For each word in the passphrase, five rolls of the dice are required. The numbers from 1 to 6 that come up in the rolls are assembled as a five digit number, e.g. 43146. That number is then used to look up a word in a word list.
+
+
+openssl rand -hex 20
+openssl rand -base64 20
+gpg --gen-random --armor 1 20
+
+https://superuser.com/questions/137957/how-to-convert-aspell-dictionary-to-simple-list-of-words
+aspell -d en dump master | aspell -l en expand | grep -v "'" |sort > wordlist_aspell_english.txt
+
+https://www.kali.org/tools/wordlists/
+https://packetstormsecurity.com/crackers/wordlists
+https://www.hackingarticles.in/wordlists-for-pentester/
+https://www.kaggle.com/datasets/taranvee/bruteforce-database-password-dictionaries
+
+
 """
 import hashlib
 import os
